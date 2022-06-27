740ab1770cf2c28c1291bec89dcd8a4ddc5b5863
Ahammad <ahammadsk1609@gmail.com>
2022-06-27 13:23:20 +0530

    Adding extra content2

--- Pract1.py
+++ Pract1.py
@@ -1173,334 +1173,239 @@ df.to_csv(filename)	Write to a CSV file
 df.to_excel(filename)	Write to an Excel file
 df.to_sql(table_name, connection_object)	Write to a SQL table
 df.to_json(filename)	Write to a file in JSON format'''
-hello
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''a = [1,2,3,4,5,6]
+df = pd.Series(a)
+#print(df)
+print(df.to_list())'''
+
+'''s1 = [2,4,6,8,10]
+s2= [1,3,5,7,9]'''
+'''se1 = pd.Series(s1)
+se2 = pd.Series(s2)
+df = se1+se2
+#print(df)
+df1 = se1*se2
+#print(df1)
+df2 = se1/se2
+print(df2)'''
+
+'''se1 = pd.Series(s1)
+se2 = pd.Series(s2)
+print(se1 == se2)
+print(se1>se2)
+print(se1<se2)'''
+
+'''d = {'a': 100, 'b': 200, 'c': 300, 'd': 400, 'e': 800}
+df = pd.Series(d)
+print(df)'''
+
+'''a = [10,20,30,40,50]
+df = pd.Series(a)
+print(df)'''
+
+'''d = pd.Series(['100', '200', 'python', '300.12', '400'])
+s = pd.to_numeric(d,errors='coerce')
+print(s)'''
+
+'''d = {'col1': [1, 2, 3, 4, 7, 11], 'col2': [4, 5, 6, 9, 5, 0], 'col3': [7, 5, 8, 12, 1,11]}
+df = pd.DataFrame(d)
+#print(df)
+d1 = df['col1']
+dd = pd.Series(d1)
+print(dd)'''
+
+'''s1 = pd.Series(['100', '200', 'python', '300.12', '400'])
+sd = np.array(s1.values.tolist())
+print(sd)'''
+
+
+'''s = pd.Series([
+    ['Red', 'Green', 'White'],
+    ['Red', 'Black'],
+    ['Yellow']])
+
+s1 = []
+
+for i,v in s.iteritems():
+    s1.extend(v)
+s = s.apply(pd.Series).stack().reset_index(drop=True)
+print(s)
+d = pd.Series(s1)
+print(d)'''
+
+'''s = pd.Series(['100', '200', 'python', '300.12', '400'])
+df = s.sort_values()
+print(df)'''
+
+'''s = pd.Series(['100', '200', 'python', '300.12', '400'])
+d = s.append(pd.Series(['500','php']))
+print(d)'''
+
+'''s = pd.Series([0,1,2,3,4,5,6,7,8,9,10])
+#print(s)
+n=6
+new_s = s[s<n]
+print(new_s)'''
+
+'''s = pd.Series([1,2,3,4,5])
+a,b = s[0],s[1]
+s[0],s[1] = b,a
+print(s)'''
+
+
+'''s = pd.Series([1,2,3,4,5,6,7,8,9,5,3])
+d = s.mean()
+print(d)
+d1 = s.std()
+print(d1)'''
+
+
+'''s1 = pd.Series([1,2,3,4,5])
+s2 = pd.Series([2,4,6,8,10])
+d = s1[~s1.isin(s2)]
+d1 = s2[~s2.isin(s1)]
+f = d.append(d1)
+print(f)'''
+
+'''s = pd.Series([1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,4,3,2,2,3,4,5,6,6,5,5,4,5,6,7,8,9,10,22,3,4,4,5,6,7,8,9,4,2,3,3])
+print(s.value_counts())'''
+
+'''s = pd.Series([1,2,3,4,5,6,7,8,9,10,12,13,15,17])
+for i,v in s.iteritems():
+    if v%5==0:
+        print(i)'''
+
+'''s = pd.Series(list('2390238923902390239023'))
+#print(s)
+ele_pos = [1,4,6,3,4,7,9,10]
+d = s.take(ele_pos)
+print(d)'''
+
+'''s1 = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+s2 = pd.Series([1, 3, 5, 7, 10])
+
+result = [pd.Index(s1).get_loc(i) for i in s2]
+print(result)'''
+
+'''s1 = pd.Series(['php', 'python', 'java', 'c#'])
+print(s1)
+result = s1.map(lambda x:x[0].upper()+x[1:-1]+x[-1].upper())
+print(result)'''
+
+'''s1 = pd.Series(['php', 'python', 'java', 'c#'])
+result = s1.map(lambda x:len(x))
+#print(result)
+s2 = pd.DataFrame({'s1':s1,'result':result})
+print(s2)'''
+
+# to get the difference.
+'''s1 = pd.Series([1, 3, 5, 8, 10, 11, 15])
+print(s1.diff().tolist())'''
+
+# convert string to date time
+'''date_series = pd.Series(['01 Jan 2015', '10-02-2016', '20180307', '2014/05/06', '2016-04-12', '2019-04-06T11:20'])
+s = pd.to_datetime(date_series)
+print(s)'''
+
+'''color_series = pd.Series(['Red', 'Green', 'Orange', 'Pink', 'Yellow', 'White'])
+a = ['a','e','i','o','u']
+for i,v in color_series.iteritems():
+    count=0
+    for j in v:
+        if j in a:
+            count +=1
+    if count >=2:
+        print(v)'''
+
+'''
+Create Test Objects
+
+pd.DataFrame(np.random.rand(20,5))	5 columns and 20 rows of random floats
+pd.Series(my_list)	Create a series from an iterable my_list
+df.index = pd.date_range('1900/1/30', periods=df.shape[0])	Add a date index
+Viewing/Inspecting Data
+
+df.head(n)	First n rows of the DataFrame
+df.tail(n)	Last n rows of the DataFrame
+df.shape	Number of rows and columns
+df.info()	Index, Datatype and Memory information
+df.describe()	Summary statistics for numerical columns
+s.value_counts(dropna=False)	View unique values and counts
+df.apply(pd.Series.value_counts)	Unique values and counts for all columns
+Selection
+
+df[col]	Returns column with label col as Series
+df[[col1, col2]]	Returns columns as a new DataFrame
+s.iloc[0]	Selection by position
+s.loc['index_one']	Selection by index
+df.iloc[0,:]	First row
+df.iloc[0,0]	First element of first column
+Data Cleaning
+
+df.columns = ['a','b','c']	Rename columns
+pd.isnull()	Checks for null Values, Returns Boolean Arrray
+pd.notnull()	Opposite of pd.isnull()
+df.dropna()	Drop all rows that contain null values
+df.dropna(axis=1)	Drop all columns that contain null values
+df.dropna(axis=1,thresh=n)	Drop all rows have have less than n non null values
+df.fillna(x)	Replace all null values with x
+s.fillna(s.mean())	Replace all null values with the mean
+s.astype(float)	Convert the datatype of the series to float
+s.replace(1,'one')	Replace all values equal to 1 with 'one'
+s.replace([2,3],['two', 'three'])	Replace all 2 with 'two' and 3 with 'three'
+df.rename(columns=lambda x: x + 1)	Mass renaming of columns
+df.rename(columns={'old_name': 'new_ name'})	Selective renaming
+df.set_index('column_one')	Change the index
+df.rename(index=lambda x: x + 1)	Mass renaming of index
+Filter, Sort, and Groupby
+
+df[df[col] > 0.6]	Rows where the column col is greater than 0.6
+df[(df[col] > 0.6) & (df[col] < 0.8)]	Rows where 0.8 > col > 0.6
+df.sort_values(col1)	Sort values by col1 in ascending order
+df.sort_values(col2,ascending=False)	Sort values by col2 in descending order.5
+df.sort_values([col1,col2],ascending=[True,False])	Sort values by col1 in ascending order then col2 in descending order
+df.groupby(col)	Returns a groupby object for values from one column
+df.groupby([col1,col2])	Returns groupby object for values from multiple columns
+df.groupby(col1)[col2]	Returns the mean of the values in col2, grouped by the values in col1
+df.pivot_table(index=col1,values=[col2,col3],aggfunc=mean)	Create a pivot table that groups by col1 and calculates the mean of col2 and col3
+df.groupby(col1).agg(np.mean)	Find the average across all columns for every unique col1 group
+df.apply(np.mean)	Apply the function np.mean() across each column
+nf.apply(np.max,axis=1)	Apply the function np.max() across each row
+Join/Combine
+
+df1.append(df2)	Add the rows in df1 to the end of df2 (columns should be identical)
+pd.concat([df1, df2],axis=1)	Add the columns in df1 to the end of df2 (rows should be identical)
+df1.join(df2,on=col1, how='inner')	SQL-style join the columns in df1 with the columns on df2 where the rows for col have identical values. The 'how' can be 'left', 'right', 'outer' or 'inner'
+Statistics
+
+df.describe()	Summary statistics for numerical columns
+df.mean()	Returns the mean of all columns
+df.corr()	Returns the correlation between columns in a DataFrame
+df.count()	Returns the number of non-null values in each DataFrame column
+df.max()	Returns the highest value in each column
+df.min()	Returns the lowest value in each column
+df.median()	Returns the median of each column
+df.std()	Returns the standard deviation of each column
+Importing Data
+
+pd.read_csv(filename)	From a CSV file
+pd.read_table(filename)	From a delimited text file (like TSV)
+pd.read_excel(filename)	From an Excel file
+pd.read_sql(query, connection_object)	Read from a SQL table/database
+pd.read_json(json_string)	Read from a JSON formatted string, URL or file.
+pd.read_html(url)	Parses an html URL, string or file and extracts tables to a list of dataframes
+pd.read_clipboard()	Takes the contents of your clipboard and passes it to read_table()
+pd.DataFrame(dict)	From a dict, keys for columns names, values for data as lists
+Exporting Data
+
+df.to_csv(filename)	Write to a CSV file
+df.to_excel(filename)	Write to an Excel file
+df.to_sql(table_name, connection_object)	Write to a SQL table
+df.to_json(filename)	Write to a file in JSON format
+
+'''
 
 
 
